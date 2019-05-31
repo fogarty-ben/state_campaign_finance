d8b3f5f64ce17141f33654632eab6fc0833c255a
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-05-31 17:19:41 -0500

    added heroku address to allowed hosts

--- state_campaign_finance/state_campaign_finance/settings.py
+++ state_campaign_finance/state_campaign_finance/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0@)m_1s(&4%x1kv-iy+%aba)hgag+im^5eno0e6nd)#cl=rt9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'state-finance-app.herokuapp.com']
 
 
 # Application definition
