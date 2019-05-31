8b0d8161c478925d26f30112610b94761d5ba2b6
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-05-31 18:21:18 -0500

    working on hello world landing page

--- state_campaign_finance/state_campaign_finance/settings.py
+++ state_campaign_finance/state_campaign_finance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0@)m_1s(&4%x1kv-iy+%aba)hgag+im^5eno0e6nd)#cl=rt9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'state-finance-app.herokuapp.com']
 
