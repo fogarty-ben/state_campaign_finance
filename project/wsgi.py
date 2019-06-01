e7d9dab8006b7659847d5e53c81efa0ba5f63626
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-01 02:13:50 -0500

    switched from django web server to gunicorn

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'state_campaign_finance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
