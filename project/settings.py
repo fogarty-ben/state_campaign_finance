5ad04e81e21b9fa38b77f6adb717ead8c69cf61e
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-05-31 20:22:25 -0500

    updated setting for new app name

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'state-finance-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'state-campaign-finance.herokuapp.com']
 
 
 # Application definition
