16f7394692fdf6da64e4765b16b27b10727f3f62
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-03 01:05:02 -0500

    workingon interface

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.template import Context
+from django.views.generic import ListView
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def search_results(request):
+    context = {'query_terms': [request.GET['query']] * 5}
+    print(request.GET)
+    return render(request, 'search.html', context)
+
