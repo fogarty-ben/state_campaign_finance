fd3802408f12f6dc652a546012d9286c0602617f
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-11 12:31:21 -0500

    search working on test data

--- app/views.py
+++ app/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from app.models import Campaignfinance
 from datetime import datetime
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def search_results(request):
-    context = {key: val for key, val in request.POST.items() if val != ''}
+    context = {key: val for key, val in request.GET.items() if val != ''}
     
     q = Campaignfinance.objects.all()
     if 'committee_name' in context:
@@ -27,18 +27,17 @@ def search_results(request):
     if 'contrib_employer' in context:
         q = q.filter(contrib_employer__search=context['contrib_full_name'])
     if 'min_amount' in context:
-        q = q.filter(amount__lte=context['min_amount'])
+        q = q.filter(amount__gte=context['min_amount'])
     if 'max_amount' in context:
-        q = q.filter(amount__gte=context['max_amount'])
+        q = q.filter(amount__lte=context['max_amount'])
     if 'start_date' in context:
         q = q.filter(date__date__gte=datetime.strptime(context['start_date'], '%Y-%m-%d'))
     if 'end_date' in context:
         q = q.filter(date__date__lte=datetime.strptime(context['end_date'], '%Y-%m-%d'))
 
-    first_rec = (context['page'] - 1) * 25
-    last_rec = (context['page'] - 1)
-
-    return render(request, 'search.html', {'results': q[first_rec : last_rec]})
+    p = Paginator(q, 25)
+    context['results'] = p.get_page(int(context.get('page', 1)))
+    return render(request, 'search.html', context)
 
 
 def contribution(request, contribution_id):
