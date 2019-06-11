18a8f186dcebb1ca46d34487f1c8bbffbc81aaab
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-11 00:24:23 -0500

    working on google cloud integration

--- project/settings.py
+++ project/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost'
+        'PORT': 5432
+        'NAME': 'postgres',
+        'USER': 'postgres'
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
