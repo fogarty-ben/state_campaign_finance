9a3fd23f29fcba3fb76fc442284860cdaa70b7f1
andrew-friedman <ajfsports1@Andrews-MacBook-Pro-5.local>
2019-06-11 18:47:04 -0500

    summary files

--- process_database/process_files.py
+++ process_database/process_files.py
@@ -6,6 +6,8 @@ from contextlib import closing
 from fake_useragent import UserAgent
 import datetime as dt
 import uuid
+from dateutil.parser import parse
+
 
 def main(filename, results="results.csv"):
     headers = requests.utils.default_headers()
@@ -28,7 +30,6 @@ def main(filename, results="results.csv"):
             for row in reader:
                 row = new_row_meta(row)
                 if row[3].lower() == "zip":
-                    print("test")
                     response = requests.get(row[1], headers=headers)
                     zipfile_load(writer, row, response)
                 elif row[3].lower() == "csv":
@@ -45,6 +46,7 @@ def new_row_meta(meta_row):
         else:
             row.append(-1)
     new_row = meta_row[0:10] + row
+    print(new_row)
     return new_row
 
 def zipfile_load(table, row, response):
@@ -108,14 +110,16 @@ def process_row(writer, meta_row, data_row):
         new_row.append(dt.datetime.strptime(data_row[meta_row[12]], meta_row[13]))
     else:
         new_row.append("")
-        print(data_row)
     for num, meta in enumerate(meta_row[14:35]):
-        if meta:
-            item = data_row[meta]
-        elif not (meta + 1):
+        if not (meta + 1):
             item = ""
+        elif meta:
+            item = data_row[meta]
         new_row.append(item)
     new_row[21] = create_name(data_row, meta_row[24], meta_row[26], meta_row[25], meta_row[23])
+    if len(new_row[14]) > 5:
+        new_row[14] = new_row[14][:5]
+    print(new_row)
     writer.writerow(new_row)
 
 
@@ -135,7 +139,102 @@ def add_name(name, data_row, num):
     if (num + 1) and data_row[num]:
         if name:
             update += " "
-        update = data_row[num]
-
+        update += data_row[num]
     return update
+
+def check_for_duplicates(filename):
+    duplicate_check = set()
+    count = 0
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            if row[2] in duplicate_check:
+                print("DUPLICATE", row[2],row[1:4])
+            if not row[2] in duplicate_check:
+                duplicate_check.add(row[2])
+            if not count % 100000:
+                print(count)
+            count += 1
+
+def check_for_numbers_errors(filename):
+    count = 0
+    last = 0
+    skipped = 0
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            count += 1
+            if not row[9]:
+                skipped += 1
+                continue
+            try:
+                int(row[9])
+            except:
+                last += 1
+                print(row[9], "|NUMBER 9|", count, last, skipped)
+            if not count % 100000:
+                print(count)
+
+
+
+
+def summary_file(filename):
+    zip_dict = {}
+    date_dict = {}
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        count = 0
+        for row in reader:
+            count += 1
+            if row[3]:
+                d_split = row[3].split(" ")
+                new_split = d_split[0].split("-")
+                date = dt.datetime(int(new_split[0]), int(new_split[1]), int(new_split[2]))
+            else:
+                date = False
+            zip_code = row[23]
+            if row[24]:
+                amount = float(row[24])
+            elif not row[24]:
+                amount = 0
+            if zip_code and zip_code not in zip_dict:
+                zip_dict[zip_code] = [1, amount]
+            elif zip_code and zip_code in zip_dict:
+                zip_dict[zip_code][0] += 1
+                zip_dict[zip_code][1] += amount
+
+            if date and date not in date_dict:
+                date_dict[date] = [1, amount]
+            elif date and date in date_dict:
+                date_dict[date][0] += 1
+                date_dict[date][1] += amount
+            if not count % 100000:
+                print(count)
+
+
+    with open("date_summary.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(["date", "count", "amount"])
+        sort = sorted(date_dict.keys())
+        for item in sort:
+            writer.writerow([item, date_dict[item][0], date_dict[item][1]])
+
+    with open("zip_summary.csv", "w") as csvfile_zip:
+        writer = csv.writer(csvfile_zip, delimiter=",")
+        writer.writerow(["date", "count", "amount"])
+        sort = sorted(zip_dict.keys())
+        for item in sort:
+            writer.writerow([item, zip_dict[item][0], zip_dict[item][1]])
+
+
+
+def load_new():
+    with open("results3.csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        with open("update_results.csv", "w") as csvfile_zip:
+            writer = csv.writer(csvfile_zip, delimiter=",")
+            next(reader)
+            for row in reader:
+                writer.writerow(row)
+
 
