0e8d1567ffc8d8bac989e09daff5925461b5900c
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-11 04:06:36 -0500

    final updates for the night

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Campaignfinance(models.Model):
+    primary_key = models.UUIDField(primary_key=True)
+    commitee_state = models.CharField(max_length=10, blank=True, null=True)
+    trans_id = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    committee_cd = models.DecimalField(max_digits=500, decimal_places=250, blank=True, null=True)
+    committee_type = models.CharField(max_length=300, blank=True, null=True)
+    commitee_name = models.CharField(max_length=300, blank=True, null=True)
+    trans_type = models.CharField(max_length=50, blank=True, null=True)
+    trans_subtype = models.CharField(max_length=50, blank=True, null=True)
+    contrib_commitee_cd = models.DecimalField(max_digits=50, decimal_places=0, blank=True, null=True)
+    contrib_org = models.CharField(max_length=300, blank=True, null=True)
+    contrib_full_name = models.CharField(max_length=300, blank=True, null=True)
+    contrib_prefix = models.CharField(max_length=300, blank=True, null=True)
+    contrib_suffix = models.CharField(max_length=300, blank=True, null=True)
+    contrib_first = models.CharField(max_length=300, blank=True, null=True)
+    contrib_middle = models.CharField(max_length=300, blank=True, null=True)
+    contrib_last = models.CharField(max_length=300, blank=True, null=True)
+    contrib_occupation = models.CharField(max_length=300, blank=True, null=True)
+    contrib_employer = models.CharField(max_length=300, blank=True, null=True)
+    contrib_addr_line_1 = models.CharField(max_length=500, blank=True, null=True)
+    contrib_addr_line_2 = models.CharField(max_length=500, blank=True, null=True)
+    contrib_city = models.CharField(max_length=500, blank=True, null=True)
+    contrib_state = models.CharField(max_length=500, blank=True, null=True)
+    contrib_zip = models.CharField(max_length=500, blank=True, null=True)
+    amount = models.DecimalField(max_digits=500, decimal_places=0, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'campaignfinance'
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from app.models import Campaignfinance
 
 
 # Create your views here.
@@ -8,10 +9,12 @@ def index(request):
 
 def search_results(request):
     context = {key: val for key, val in request.POST.items() if val != ''}
-    print(context)
+    non_name = {key: val for key, val in request.POST.items() if key not in ['committee_name', 'committee_name', 'csrfmiddlewaretoken']}
+    q1 = Campaignfinance.objects.filter(**non_name)
+    print(q1)
     return render(request, 'search.html', context)
 
 
 def contribution(request, contribution_id):
-    #contribution = get_object_or_404(Contribution, pk=primary_key)
-    return render(request, 'contribution.html', {'contribution_id': contribution_id})
+    contribution = get_object_or_404(Campaignfinance, pk=contribution_id)
+    return render(request, 'contribution.html', {'contribution': contribution})
