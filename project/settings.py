1e884a06e25201418286ba5d4eb9eb2bdcd38345
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-11 00:26:46 -0500

    minor change to database settings

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': { 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'localhost'
+        'HOST': 'localhost',
         'PORT': '5432',
         'NAME': 'postgres',
         'USER': 'postgres'
