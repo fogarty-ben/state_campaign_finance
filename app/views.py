52cf4aae17a872b7e3e82ac91b51d8e466076e53
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-11 21:08:56 -0500

    finalizing search

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import Q
+from django.contrib.postgres.search import SearchQuery, SearchVector
+
 
 # Create your views here.
 def index(request):
@@ -22,32 +24,85 @@ def interested(request):
 def search_results(request):
     context = {key: val for key, val in request.GET.items() if val != ''}
     
-    q = contributions.objects.all()
-    if 'committee_name' in context:
-        q = q.filter(committee_name__search=context['committee_name'])
+    query = """
+                SELECT *
+                FROM financelarge
+                WHERE {}
+                """
+    where_clause = []
+    params = {}
     if 'committee_state' in context:
-        q = q.filter(committee_state=context['committee_state'])
-    if 'contrib_name' in context:
-        q = q.filter(Q(contrib_first__search=context['contrib_name']) |
-                     Q(contrib_middle__search=context['contrib_name'])  |
-                     Q(contrib_last__search=context['contrib_name'])  |
-                     Q(contrib_org__search=context['contrib_name']) )
+        append = """
+                 (committee_state = %(committee_state)s)
+                 """
+        where_clause.append(append)
+        params['committee_state'] = context['committee_state']
     if 'contrib_zip' in context:
-        q = q.filter(contrib_zip=context['contrib_zip'])
+        append = """
+                 (committee_zip = %(committee_zip)s)
+                 """
+        where_clause.append(append)
+        params['committee_zip'] = context['committee_zip']
     if 'contrib_state' in context:
-        q = q.filter(contrib_state=context['contrib_state'])
-    if 'contrib_occupation' in context:
-        q = q.filter(contrib_occupation__search=context['committee_name'])
-    if 'contrib_employer' in context:
-        q = q.filter(contrib_employer__search=context['contrib_employer'])
+        append = """
+                 (contrib_state = %(contrib_state)s)
+                 """
+        where_clause.append(append)
+        params['contrib_state'] = context['contrib_state']
     if 'min_amount' in context:
-        q = q.filter(amount__gte=context['min_amount'])
+        append = """
+                 (amount >= %(min_amount)s)
+                 """
+        where_clause.append(append)
+        params['min_amount'] = int(context['min_amount'])
     if 'max_amount' in context:
-        q = q.filter(amount__lte=context['max_amount'])
+        append = """
+                 (amount <= %(max_amount)s)
+                 """
+        where_clause.append(append)
+        params['max_amount'] = context['max_amount']
     if 'start_date' in context:
-        q = q.filter(date__gte=datetime.strptime(context['start_date'], '%Y-%m-%d').date())
+        append = """
+                 (amount >= DATE(%(start_date)s))
+                 """
+        where_clause.append(append)
+        params['start_date'] = context['start_date']
     if 'end_date' in context:
-        q = q.filter(date__lte=datetime.strptime(context['end_date'], '%Y-%m-%d').date())
+        append = """
+                 (amount <= DATE(%(end_date)s))
+                 """
+        where_clause.append(append)
+        params['end_date'] = context['end_date']
+    if 'committee_name' in context:
+        append = """
+                 (to_tsvector('english', committee_name) @@ plainto_tsquery('english', %(committee_name)s))
+                 """
+        where_clause.append(append)
+        params['committee_name'] = context['committee_name']
+    if 'contrib_name' in context:
+        append = """
+                (to_tsvector('english', contrib_first) @@ plainto_tsquery('english', %(contrib_name)s) OR
+                to_tsvector('english', contrib_middle) @@ plainto_tsquery('english', %(contrib_name)s) OR
+                to_tsvector('english', contrib_last) @@ plainto_tsquery('english', %(contrib_name)s) OR
+                to_tsvector('english', contrib_org) @@ plainto_tsquery('english', %(contrib_name)s))
+                """
+        where_clause.append(append)
+        params['contrib_name'] = context['contrib_name']
+    if 'contrib_occupation' in context:
+        append = """
+                 (to_tsvector('english', contrib_occupation) @@ plainto_tsquery('english', %(contrib_occupation)s))
+                 """
+        where_clause.append(append)
+        params['contrib_occupation'] = context['contrib_occupation']
+    if 'contrib_employer' in context:
+        append = """
+                 (to_tsvector('english', contrib_employer) @@ plainto_tsquery('english', %(contrib_employer)s))
+                 """
+        where_clause.append(append)
+        params['contrib_employer'] = context['contrib_employer']
+    where_clause = " AND ".join(where_clause)
+    q = contributions.objects\
+                     .raw(query.format(where_clause), params)
 
     p = Paginator(q, 25)
     context['results'] = p.get_page(int(context.get('page', 1)))
